e63ca9614ab9381a6f1daabfcf7f981ac1e3460b
unknown <adam.barnett23@gmail.com>
2025-05-07 13:23:37 -0400

    adding the detection, along with elements to allow simpler local testing

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,12 @@
+import time
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 import threading
 import base64
 import cv2
+import keyboard
 
 app = FastAPI()
 
@@ -16,11 +19,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+image_data = []  # Stores latest frame (in base 64 as a json string ready to display in react)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+smile_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
+stop_event = threading.Event()
+# two colorblind friendly colors
+smiles_col = (213, 94, 0)
+faces_col = (34, 113, 178)
 
-image_data = []  # Stores latest frame (in base 64)
-
-def primary_backend_loop():
-    cap = cv2.VideoCapture(0)  # 0 = default camera
+def primary_backend_loop(display):
+    cap = cv2.VideoCapture(0)  # 0 = default camera (future work: check available cameras and offer options to frontend)
 
     if not cap.isOpened():
         print("Could not open webcam.")
@@ -30,24 +38,71 @@ def primary_backend_loop():
         ret, frame = cap.read()
         if not ret:
             continue
-
-        ret, jpeg = cv2.imencode('.jpg', frame)
+        # detecting then adding the faces and smiles
+        [faces, smiles] = find_faces_and_smiles(frame)
+        faces_image = add_boxes_to_image(frame, faces, faces_col)
+        smiles_image = add_boxes_to_image(faces_image, smiles, smiles_col)
+        if display:
+            cv2.imshow('testing_backend', smiles_image)
+            if cv2.waitKey(1) == 32: #space bar
+                break
+        ret, jpeg = cv2.imencode('.jpg', smiles_image)
         if ret:
             # storing the frame in base 64 was necessary to get it from Python to react
             b64 = base64.b64encode(jpeg.tobytes()).decode('utf-8')
             image_data.clear()
             image_data.append(f"data:image/jpeg;base64,{b64}")
+        if stop_event.is_set():
+            break
     cap.release()
+    cv2.destroyAllWindows()
 
 """ note this is a deprecated method, kept for now, hopefully will have time to replace it
-correct thing to use is lifespan events, though there may also be some logic to shifting the code in
- this file over to a class and using __init__ and __del__"""
+correct thing to use is lifespan events (https://fastapi.tiangolo.com/advanced/events/#lifespan )
+there may also be some logic to shifting the code in this file over to a class and using __init__ and __del__"""
 @app.on_event("startup")
-def start_backend_thread():
-    thread = threading.Thread(target=primary_backend_loop, daemon=True)
+def start_backend_thread(testing_locally=False):
+    thread = threading.Thread(target=primary_backend_loop, daemon=True, args=[testing_locally])
     thread.start()
 
-@app.get("/images")
+@app.get("/image")
 def get_current_image():
     # future work: might be worth using a lock on this data here and above where it is accessed
     return JSONResponse(content=image_data.copy())
+
+""" notes of things to try in here:
+- different input values to the different cascade detections
+- the official documents recommend equalising the histogram of the image:
+   equ = cv2.equalizeHist(img)
+   but they are very old, test the usefulness of this
+- do some verification and testing of the scale factor and minNeighbours input options"""
+def find_faces_and_smiles(frame):
+    grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(grayscale_frame, 1.3, 5)
+    full_smiles = []
+    for (x, y, w, h) in faces:
+        grayscale_face = grayscale_frame[y:y + h, x:x + w]
+        local_smiles = smile_cascade.detectMultiScale(grayscale_face, 1.8, 20)
+        for (x_smile, y_smile, w_smile, h_smile) in local_smiles:
+            full_smiles.append((x_smile + x, y_smile + y, w_smile, h_smile))
+    return faces, tuple(full_smiles)
+
+def add_boxes_to_image(image, boxes, color):
+    for (x, y, w, h) in boxes:
+        cv2.rectangle(image, (x, y), ((x + w), (y + h)), color, 2)
+    return image
+
+def local_testing():
+    start_backend_thread(True)
+    # Need to start primary backend loop (AND stop it when we exit)
+    while True:
+        if keyboard.read_key() == 'space':
+            print('exiting')
+            stop_event.set()
+            break
+
+
+
+if __name__ == "__main__":
+    # then we are testing this with no frontend so we run it locally
+    local_testing()
